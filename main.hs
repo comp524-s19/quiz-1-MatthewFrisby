fb36f3ae6eb68efcab1842b956fb5ef37dcac69c
Matthew Frisby <matt21@live.unc.edu>
2019-01-31 18:00:12 +0000

    sure

--- main.hs
+++ main.hs
@@ -1,5 +1,2 @@
-
 finalGrade :: [Int] -> [Int] -> Int
-finalGrade grade weight = top/bottom
-        let top = sum (zipWith (*) grade weight)
-	let bottom = sum (weight)
+finalGrade grade weight = (sum (zipWith (*) grade weight)) `div` (sum (weight))
